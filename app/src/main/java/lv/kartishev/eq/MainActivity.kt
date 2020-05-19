fe25899d155fff68d2b2b97447896afcc9afec68
Alex Kartishev <alexkartishev@gmail.com>
2020-05-19 15:44:15 +0300

    add navigation

--- app/src/main/java/lv/kartishev/eq/MainActivity.kt
+++ app/src/main/java/lv/kartishev/eq/MainActivity.kt
@@ -5,16 +5,25 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.button.MaterialButtonToggleGroup
+import com.google.android.material.navigation.NavigationView
 import com.rm.rmswitch.RMTristateSwitch
 
 
@@ -27,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var checkButton: MaterialButton;
     var mPluginReceiver: BroadcastReceiver = PluginBroadcastReceiver()
     lateinit var mAdView: AdView;
+    lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onResume() {
         super.onResume()
@@ -55,6 +65,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         MobileAds.initialize(this) { }
 
@@ -146,6 +159,27 @@ class MainActivity : AppCompatActivity() {
                 playing = true
             }
         }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home, R.id.nav_second), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     inner class PluginBroadcastReceiver : BroadcastReceiver() {
